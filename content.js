ff7aeb18a7ae3cb3d43a2d62e7567194c3390524
areyouahobo <wj310360@gmail.com>
2017-11-13 15:08:44 -0600

    Added auto-update function

--- content.js
+++ content.js
@@ -1,5 +1,19 @@
 var selectedSchedule;
 
+chrome.runtime.onUpdateAvailable.addListener(function(details) {
+  console.log("updating to version " + details.version);
+  chrome.runtime.reload();
+});
+
+chrome.runtime.requestUpdateCheck(function(status) {
+  if (status == "update_available") {
+    console.log("update pending...");
+  } else if (status == "no_update") {
+    console.log("no update found");
+  } else if (status == "throttled") {
+    console.log("Oops, I'm asking too frequently - I need to back off.");
+  }
+});
 
 $(function checkSettings() {
   chrome.storage.sync.get('scheduleToLoad', function(data) {
